53724df8a82dae36a271a33668f57987b92f1bb4
mysticBel <dnxbel@gmail.com>
2023-07-08 17:08:00 -0500

    creando model Producto

---
+++ sesion02/Controllers/NegociosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+
+namespace sesion02.Controllers
+{
+    public class NegociosController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ sesion02/Models/Producto.cs
@@ -0,0 +1,11 @@
+﻿namespace sesion02.Models
+{
+    public class Producto
+    {
+        public int idProducto { get; set; }
+        public string? nombreProducto { get; set; }
+        public string? nombreCategoria { get; set; }
+        public decimal precioUnitario { get; set; }
+        public int stock { get; set; }
+    }
+}
