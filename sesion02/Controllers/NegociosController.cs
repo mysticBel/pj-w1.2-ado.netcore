cbf48a36e1a69e6a9b7f4ad57cfbab5f02adeed5
mysticBel <dnxbel@gmail.com>
2023-07-08 17:58:16 -0500

    agregando metodo en controlador y vista para listar productos (join)

--- sesion02/Controllers/NegociosController.cs
+++ sesion02/Controllers/NegociosController.cs
@@ -2,6 +2,9 @@
 
 //para la cadena de conexion
 using Microsoft.Extensions.Configuration;
+//
+using Microsoft.Data.SqlClient;
+using sesion02.Models;
 
 namespace sesion02.Controllers
 {
@@ -19,7 +22,38 @@ namespace sesion02.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(GetProductos());
+        }
+
+        // usando microsoft.dATA
+        IEnumerable<Producto> GetProductos() 
+        { 
+            List<Producto> productos = new List<Producto>();
+
+            using (SqlConnection connection = new SqlConnection(cadena)) 
+            {
+                SqlCommand cmd = new SqlCommand("SELECT P.idProducto, " +
+                                "  P.nombreProducto, C.nombreCategoria, " +
+                                "  P.precioUnitario, P.stock\r\n" +
+                                "  FROM PRODUCTO P\r\n" +
+                 "  INNER JOIN CATEGORIA C ON C.idCategoria = P.idCategoria", connection);
+                //
+                connection.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    productos.Add(new Producto()
+                    {
+                        idProducto = dr.GetInt32(0),
+                        nombreProducto = dr.GetString(1),
+                        nombreCategoria= dr.GetString(2),
+                        precioUnitario = dr.GetDecimal(3),
+                        stock = dr.GetInt32(4)
+                    });
+                }
+            }
+
+            return productos;
         }
     }
 }
