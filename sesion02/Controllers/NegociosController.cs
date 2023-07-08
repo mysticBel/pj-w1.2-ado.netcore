c78e92f351578f0e9e07b15b0bc60e8076c8c153
mysticBel <dnxbel@gmail.com>
2023-07-08 17:21:48 -0500

    cadena de conexion al controller

--- sesion02/Controllers/NegociosController.cs
+++ sesion02/Controllers/NegociosController.cs
@@ -1,11 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-
+//para la cadena de conexion
+using Microsoft.Extensions.Configuration;
 
 namespace sesion02.Controllers
 {
     public class NegociosController : Controller
     {
+
+        //
+        public readonly IConfiguration _config;
+        public string cadena;
+        public NegociosController(IConfiguration _config)
+        {
+            this._config = _config;  //hace referencia al 2do config, this config a la 1er(var global)
+            this.cadena = _config["ConnectionStrings:connection"]; //obtenemos la cadena de appsettings.json
+        }
+
         public IActionResult Index()
         {
             return View();
