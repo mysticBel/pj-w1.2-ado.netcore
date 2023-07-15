7c421c8fde320bbb0e7db207260873ebe3daeea9
mysticBel <dnxbel@gmail.com>
2023-07-14 20:04:44 -0500

    data annotations

--- sesion02/Models/Producto.cs
+++ sesion02/Models/Producto.cs
@@ -1,11 +1,19 @@
-﻿namespace sesion02.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace sesion02.Models
+
+
 {
     public class Producto
     {
+        [Display(Name="ID")]
         public int idProducto { get; set; }
+        [Display(Name = "Nombre")]
         public string? nombreProducto { get; set; }
+        [Display(Name = "Nombre Categoria")]
         public string? nombreCategoria { get; set; }
+        [Display(Name = "Precio Unitario")]
         public decimal precioUnitario { get; set; }
+        [Display(Name = "Stock")]
         public int stock { get; set; }
     }
 }
